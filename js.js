3198d807fc32cdd70e17469522bf85a14476f133
maylen aguiar <maylenaguiar.psi@gmail.com>
2021-09-09 17:55:30 -0300

    tercer desafio

---
+++ js.js
@@ -0,0 +1,57 @@
+let mesCumple = parseInt(prompt ("ingresar el número del mes de tu nacimiento"));
+
+    switch (mesCumple) {
+        case 1:
+        alert( 'Naciste en enero');
+        break;
+    
+        case 2:
+        alert( 'Naciste en febrero');
+        break;
+
+        case 3:
+        alert( 'Naciste en marzo');
+        break;
+
+        case 4:
+        alert( 'Naciste en abril');
+        break;
+
+        case 5:
+        alert( 'Naciste en mayo');
+        break;
+
+        case 6:
+        alert( 'Naciste en junio');
+        break;
+
+        case 7:
+        alert( 'Naciste en julio');
+        break;
+
+        case 8:
+        alert( 'Naciste en agosto');
+        break;
+
+        case 9:
+        alert( 'Naciste en septiembre');
+        break;
+    
+        case 10:
+        alert( 'Naciste en octubre');
+        break;
+
+        case 11:
+        alert( 'Naciste en noviembre');
+        break;
+    
+        case 12:
+        alert( 'Naciste en diciembre');
+        break;
+
+        default:
+        alert ('No es un número válido para un mes');
+        break;
+    }
+
+
